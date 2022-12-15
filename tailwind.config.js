cf397c3ae31586b066461d9bad62befffbc1439d
Lanre Abdulkareem <lanre@Apples-MacBook-Pro.local>
2022-12-15 12:59:47 -0600

    body content done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'back-image': "url('./src/assets/joakim-honkasalo-dyHHrEtrrh4-unsplash.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
